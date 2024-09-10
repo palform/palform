7f2746b0559795d4bb3ba7fcedcd690bdce93ba8
Pal Kerecsenyi <pal@p.kerecs.com>
2024-09-10 20:29:18 +0100

    Add sitemap generator for astro

--- packages/landing/astro.config.mjs
+++ packages/landing/astro.config.mjs
@@ -2,9 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [svelte(), tailwind(), icon()],
+    integrations: [
+        svelte(),
+        tailwind(),
+        icon(),
+        sitemap({
+            changefreq: "weekly",
+        }),
+    ],
     site: "https://palform.app",
 });
