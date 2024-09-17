a801e7c7eb4a30d51e0bf89e1c19cb74526b8f8e
Pal Kerecsenyi <pal@p.kerecs.com>
2024-09-17 18:15:36 +0100

    Changes to landing page hero header

--- packages/frontend/src/components/forms/fill/FormFillLoading.svelte
+++ packages/frontend/src/components/forms/fill/FormFillLoading.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
     import { fade } from "svelte/transition";
     import BusyProgressBar from "../../BusyProgressBar.svelte";
-    import PalformLogo from "../../palform/PalformLogo.svelte";
 </script>
 
 <div
-    class="fixed top-0 left-0 z-50 w-screen h-screen bg-gray-50/50 dark:bg-slate-900 flex flex-col gap-4 justify-center items-center"
+    class="fixed top-0 left-0 z-50 w-screen h-screen bg-gray-50/50 dark:bg-slate-900 flex flex-col justify-center items-center"
     out:fade={{ duration: 300 }}
 >
-    <PalformLogo class="h-8" hideAlt={true} />
     <BusyProgressBar />
 </div>
