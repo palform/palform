e8d486439683cd281bffcf4a32b93c12a06ac46a
Pal Kerecsenyi <pal@p.kerecs.com>
2024-11-22 16:18:32 +0000

    Squash merge navbar-overhaul into main

---
+++ packages/landing/src/components/navbar/NavSubItem.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let subItem: { title: string; description: string; href: string };
+</script>
+
+<a
+    href={subItem.href}
+    class="block px-4 py-3 rounded-lg hover:bg-slate-200/50 dark:hover:bg-slate-700 transition-colors"
+>
+    <span class="font-medium text-primary-800 dark:text-gray-200 block mb-2 leading-none"
+        >{subItem.title}</span
+    >
+    <span
+        class="block text-sm text-primary-800/70 dark:text-gray-400 font-normal font-sans leading-tight"
+    >
+        {subItem.description}
+    </span>
+</a>
---
+++ packages/landing/src/components/navbar/Navbar.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import logo from "../../assets/logo_type.svg?raw";
+    import NavItem from "./NavItem.svelte";
+</script>
+
+<nav class="fixed w-full flex justify-center pt-2 z-10">
+    <div class="px-6 md:px-12 xl:px-20 max-w-7xl w-full">
+        <div
+            class="h-12 px-8 flex justify-between items-center bg-slate-100/80 dark:bg-slate-800/80 backdrop-blur-lg rounded-full border border-slate-400/40"
+        >
+            <p class="transition-transform hover:scale-105 active:scale-95">
+                <a href="/#">
+                    <svg class="h-7" viewBox="0 0 304.07928 85.568403">
+                        {@html logo}
+                    </svg>
+                </a>
+            </p>
+            <ul
+                class="flex items-center gap-4 sm:gap-8 dark:text-gray-200 font-display font-light text-primary-800"
+            >
+                <NavItem
+                    desktopOnly
+                    subItems={[
+                        {
+                            title: "End-to-end encryption",
+                            description:
+                                "No-one can see your responses. Not even us. Keep your data more secure than ever with Palform.",
+                            href: "/features/encryption",
+                        },
+                        {
+                            title: "Open source",
+                            description:
+                                "Accessible and easily auditable code licensed under AGPL. All development is done in public.",
+                            href: "https://github.com/palform/palform",
+                        },
+                        {
+                            title: "Customisable branching",
+                            description:
+                                "Personalise your forms with recall and branching, keeping your users super engaged throughout.",
+                            href: "https://docs.palform.app/forms/branching/",
+                        },
+                        {
+                            title: "Response notifications",
+                            description:
+                                "Connect your app to a range of services using webhook and email notifications",
+                            href: "https://docs.palform.app/forms/notifications/",
+                        },
+                    ]}
+                >
+                    Features
+                </NavItem>
+                <NavItem href="/templates" desktopOnly>Templates</NavItem>
+                <NavItem href="/#pricing" desktopOnly>Pricing</NavItem>
+                <NavItem href="https://dash.palform.app/auth/login">
+                    Sign in
+                </NavItem>
+            </ul>
+        </div>
+    </div>
+</nav>
